fe3bcf839e231cd67f08afe29bd455b2f45d89f0
Tianhao Lei <254130357@qq.com>
2021-02-05 08:59:48 -0600

    added a really basic preview function.

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from setup import ag, status
-import initialStatus
-import callbacks
-from socketApp import socketio, app as sock
-from httpServer import app as server
+from behavior_gui.setup import ag, status
+import behavior_gui.initialStatus
+import behavior_gui.callbacks
+from behavior_gui.socketApp import socketio, app as sock
+from behavior_gui.httpServer import app as server
 from threading import Thread
 
 if __name__ == "__main__":
-  Thread(target=socketio.run, args=(sock,), kwargs={'port': 5001}).start()
-  server.run(host='localhost', port=5000, debug=False,
-             use_reloader=False)
+  # Thread(target=socketio.run, args=(sock,), kwargs={'port': 5001}).start()
+  # server.run(host='localhost', port=5000, debug=False,
+  #            use_reloader=False)
+  socketio.run(sock, port=5001)
