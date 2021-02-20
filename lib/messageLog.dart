20a391ff2d2646a844888097cf391361a3167079
zfj1 <39531155+zfj1@users.noreply.github.com>
2021-02-20 13:19:11 -0600

    converting message log to dynamic listview

--- lib/messageLog.dart
+++ lib/messageLog.dart
@@ -7,18 +7,38 @@ void showMessageLog(BuildContext context) async {
       builder: (BuildContext context) {
         ThemeData theme = Theme.of(context);
         return SimpleDialog(
-          title: Center(
-              child: Text('Message Log',
-                  style: TextStyle(color: theme.buttonColor))),
-          children: Api.messageQueue.map<Widget>((text) {
-            return SimpleDialogOption(
-                // onPressed: () => Navigator.pop(context),
-                child: Row(children: [
-              Text('[${text.key}] ',
-                  style: TextStyle(color: theme.buttonColor)),
-              Text(text.value, style: TextStyle(color: theme.primaryColor))
-            ]));
-          }).toList(),
-        );
+            title: Center(
+                child: Text('Message Log',
+                    style: TextStyle(color: theme.buttonColor))),
+            children: [
+              StreamBuilder(
+                  stream: Api.onMessage,
+                  initialData: null,
+                  builder: (context, snapshot) {
+                    return ListView.builder(
+                      itemCount: Api.messageQueue.length,
+                      itemBuilder: (context, index) {
+                        MapEntry<DateTime, String> entry =
+                            Api.messageQueue[index];
+                        return Row(children: [
+                          Text('[${entry.key}] ',
+                              style: TextStyle(color: theme.buttonColor)),
+                          Text(entry.value,
+                              style: TextStyle(color: theme.primaryColor))
+                        ]);
+                      },
+                    );
+                  })
+            ]
+            //   Api.messageQueue.map<Widget>((text) {
+            //     return SimpleDialogOption(
+            //         // onPressed: () => Navigator.pop(context),
+            //         child: Row(children: [
+            //       Text('[${text.key}] ',
+            //           style: TextStyle(color: theme.buttonColor)),
+            //       Text(text.value, style: TextStyle(color: theme.primaryColor))
+            //     ]));
+            //   }).toList(),
+            );
       });
 }
