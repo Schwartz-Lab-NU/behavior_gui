53fd6066ff61bfb26034a2a5b1655b16070407b7
Zach <z.jessen@gmail.com>
2021-08-09 16:14:43 -0500

    fixed debug

--- lib/settings.dart
+++ lib/settings.dart
@@ -436,7 +436,7 @@ class _StatusBarState extends State<StatusBar> with TickerProviderStateMixin {
       });
 
       DatabaseStatus _databaseStatus = DatabaseStatus();
-      debugPrint(_databaseStatus.keys());
+      debugPrint(_databaseStatus.keys);
     });
 
     try {
